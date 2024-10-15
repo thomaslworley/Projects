b7a9166a2f12e12d7600d225dd515559f70e7049
thomaslworley <42894068+thomaslworley@users.noreply.github.com>
2024-10-15 15:23:23 -0500

    Changed modules and added Intents line to align with discord API changes

--- pharmbot.py
+++ pharmbot.py
@@ -1,7 +1,10 @@
-import discord, random, api
+import discord
 from discord.ext import commands
+import api
 
-client = commands.Bot(command_prefix= '/')
+intents= discord.Intents.default()
+
+client = commands.Bot(intents=intents, command_prefix= '/')
 
 @client.event
 async def on_ready():
