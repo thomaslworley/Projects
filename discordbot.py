fd4acc8aec37c3ad36b1837edc4497b445b26d5f
thomaslworley <42894068+thomaslworley@users.noreply.github.com>
2024-10-16 17:59:56 -0500

    Add files via upload

---
+++ discordbot.py
@@ -0,0 +1,34 @@
+"""Discord module allows to connect to Discord Python API to run and code the bot's functions
+   And api module holds the API key for the bot without leaving it in the code"""
+import discord
+from discord.ext import commands
+import api
+
+
+def main():
+    """Main function just to run the bot and the events it will handle and commands to execute"""
+    intents = discord.Intents.default()
+    client = commands.Bot(command_prefix='!', intents=intents)
+
+    log_channel = 1296240616685961237
+
+    @client.event
+    async def on_ready():
+        print('The bot is running!')
+        online = discord.Status.online
+        custom = discord.CustomActivity
+        channel = client.get_channel(log_channel)
+        await client.change_presence(status=online, activity=custom(name='Currently being Wacky!'))
+        await channel.send('Bot is running!')
+
+
+    client.run(api.Key)
+
+
+
+
+
+
+
+if __name__=='__main__':
+    main()
